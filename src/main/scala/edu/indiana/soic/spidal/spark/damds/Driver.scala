f3567c569247c4ce6a6038566276a66e63149673
pulasthi <pulasthi911@gmail.com>
2016-01-10 18:04:21 -0500

    adding changes

--- src/main/scala/edu/indiana/soic/spidal/spark/damds/Driver.scala
+++ src/main/scala/edu/indiana/soic/spidal/spark/damds/Driver.scala
@@ -12,8 +12,8 @@ import java.util.regex.Pattern
 
 import com.google.common.base.{Optional, Stopwatch, Strings}
 import edu.indiana.soic.spidal.common.{BinaryReader2D, WeightsWrap, _}
-import edu.indiana.soic.spidal.damds.Utils
-import edu.indiana.soic.spidal.damds.Utils
+//import edu.indiana.soic.spidal.damds.Utils
+//import edu.indiana.soic.spidal.damds.Utils
 import edu.indiana.soic.spidal.spark.configurations._
 import edu.indiana.soic.spidal.spark.configurations.section._
 import org.apache.commons.cli.{Options, _}
@@ -37,15 +37,15 @@ object Driver {
   programOptions.addOption(Constants.CmdOptionShortT.toString, Constants.CmdOptionLongT.toString, true, Constants.CmdOptionDescriptionT.toString)
 
   /**
-   * Weighted SMACOF based on Deterministic Annealing algorithm
-   *
-   * @param args command line arguments to the program, which should include
-   *             -c path to config file
-   *             -t number of threads
-   *             -n number of nodes
-   *             The options may also be given as longer names
-   *             --configFile, --threadCount, and --nodeCount respectively
-   */
+    * Weighted SMACOF based on Deterministic Annealing algorithm
+    *
+    * @param args command line arguments to the program, which should include
+    *             -c path to config file
+    *             -t number of threads
+    *             -n number of nodes
+    *             The options may also be given as longer names
+    *             --configFile, --threadCount, and --nodeCount respectively
+    */
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("sparkMDS").setMaster("local")
     val sc = new SparkContext(conf)
@@ -77,7 +77,7 @@ object Driver {
     try {
       readConfigurations(cmd)
       config.distanceMatrixFile = "/home/pulasthi/work/sparkdamds/whiten_dataonly_fullData.2320738e24c8993d6d723d2fd05ca2393bb25fa4.4mer.dist.c#_1000.bin"
-      val ranges: Array[Range] = RangePartitioner.Partition(0,1000,1)
+      val ranges: Array[Range] = RangePartitioner.Partition(0, 1000, 1)
       ParallelOps.procRowRange = ranges(0);
       readDistancesAndWeights(config.isSammon);
 
@@ -136,169 +136,174 @@ object Driver {
 
 
 
-      print("Asd")
-    } catch {
+        print("Asd")
+      } catch {
       case e: Exception => {
         e.printStackTrace
       }
     }
 
 
-  }
-
-  def matrixToIndexRow(matrix: Array[Array[Short]]): Array[IndexedRow] = {
-    matrix.zipWithIndex.map { case (row, i) => new IndexedRow(i, new DenseVector(row.map(_.toDouble))) };
-  }
-
-  def parseCommandLineArguments(args: Array[String], opts: Options): Optional[CommandLine] = {
-    val optParser: CommandLineParser = new GnuParser();
-    try {
-      return Optional.fromNullable(optParser.parse(opts, args))
     }
-    catch {
-      case e: ParseException => {
-        e.printStackTrace
+
+    def matrixToIndexRow(matrix: Array[Array[Short]]): Array[IndexedRow] = {
+      matrix.zipWithIndex.map { case (row, i) => new IndexedRow(i, new DenseVector(row.map(_.toDouble))) };
+    }
+
+    def parseCommandLineArguments(args: Array[String], opts: Options): Optional[CommandLine] = {
+      val optParser: CommandLineParser = new GnuParser();
+      try {
+        return Optional.fromNullable(optParser.parse(opts, args))
       }
+      catch {
+        case e: ParseException => {
+          e.printStackTrace
+        }
+      }
+      return Optional.fromNullable(null);
     }
-    return Optional.fromNullable(null);
-  }
 
-  private def changeZeroDistancesToPostiveMin(distances: Array[Array[Short]], positiveMin: Double) {
-    var tmpD: Double = 0.0
-    for (distanceRow <- distances) {
-      for (i <- 0 until distanceRow.length) {
-        tmpD = distanceRow(i) * 1.0 / Short.MaxValue
-        if (tmpD < positiveMin && tmpD >= 0.0) {
-          distanceRow(i) = (positiveMin * Short.MaxValue).toShort
+    def changeZeroDistancesToPostiveMin(distances: Array[Array[Short]], positiveMin: Double)
+    {
+      var tmpD: Double = 0.0
+      for (distanceRow <- distances) {
+        for (i <- 0 until distanceRow.length) {
+          tmpD = distanceRow(i) * 1.0 / Short.MaxValue
+          if (tmpD < positiveMin && tmpD >= 0.0) {
+            distanceRow(i) = (positiveMin * Short.MaxValue).toShort
+          }
         }
       }
     }
-  }
 
-  //TODO need to test method
-  private def readInitMapping(initialPointsFile: String, numPoints: Int, targetDimension: Int): Array[Array[Double]] = {
-    try {
-      var x: Array[Array[Double]] = Array.ofDim[Double](numPoints,targetDimension);
-      var line: String = null
-      val pattern: Pattern = Pattern.compile("[\t]")
-      var row: Int = 0
-      for (line <- Source.fromFile(initialPointsFile).getLines()) {
-        if (!Strings.isNullOrEmpty(line)) {
-          val splits: Array[String] = pattern.split(line.trim)
+    //TODO need to test method
+    def readInitMapping(initialPointsFile: String, numPoints: Int, targetDimension: Int): Array[Array[Double]] =
+    {
+      try {
+        var x: Array[Array[Double]] = Array.ofDim[Double](numPoints, targetDimension);
+        var line: String = null
+        val pattern: Pattern = Pattern.compile("[\t]")
+        var row: Int = 0
+        for (line <- Source.fromFile(initialPointsFile).getLines()) {
+          if (!Strings.isNullOrEmpty(line)) {
+            val splits: Array[String] = pattern.split(line.trim)
 
-          for (i <- 0 until splits.length) {
-            x(row)(i) = splits(i).trim.toDouble
+            for (i <- 0 until splits.length) {
+              x(row)(i) = splits(i).trim.toDouble
+            }
+            row += 1;
           }
-          row += 1;
+        }
+        return x;
+      } catch {
+        case ex: IOException => throw new RuntimeException(ex)
+      }
+    }
+
+    //TODO need to test method
+    def generateInitMapping(numPoints: Int, targetDim: Int): Array[Array[Double]] =
+    {
+      var x: Array[Array[Double]] = Array.ofDim[Double](numPoints, targetDim);
+      val rand: Random = new Random(System.currentTimeMillis)
+      for (row <- x) {
+        for (i <- 0 until row.length) {
+          row(i) = if (rand.nextBoolean) rand.nextDouble else -rand.nextDouble
         }
       }
       return x;
-    } catch {
-      case ex: IOException => throw new RuntimeException(ex)
     }
-  }
 
-  //TODO need to test method
-  private def generateInitMapping(numPoints: Int, targetDim: Int): Array[Array[Double]] = {
-    var x: Array[Array[Double]] = Array.ofDim[Double](numPoints,targetDim);
-    val rand: Random = new Random(System.currentTimeMillis)
-    for (row <- x) {
-      for (i <- 0 until row.length) {
-        row(i) = if (rand.nextBoolean) rand.nextDouble else -rand.nextDouble
+    def readConfigurations(cmd: CommandLine): Unit = {
+      Driver.config = ConfigurationMgr.LoadConfiguration(
+        cmd.getOptionValue(Constants.CmdOptionLongC)).damdsSection;
+      //TODO check if this is always correct
+      ParallelOps.globalColCount = config.numberDataPoints;
+      ParallelOps.nodeCount =
+        Integer.parseInt(cmd.getOptionValue(Constants.CmdOptionLongN));
+      ParallelOps.threadCount =
+        Integer.parseInt(cmd.getOptionValue(Constants.CmdOptionLongT));
+      ParallelOps.mmapsPerNode = if (cmd.hasOption(Constants.CmdOptionShortMMaps)) cmd.getOptionValue(Constants.CmdOptionShortMMaps).toInt else 1;
+      ParallelOps.mmapScratchDir = if (cmd.hasOption(Constants.CmdOptionShortMMapScrathDir)) cmd.getOptionValue(Constants.CmdOptionShortMMapScrathDir) else ".";
+
+      Driver.byteOrder =
+        if (Driver.config.isBigEndian) ByteOrder.BIG_ENDIAN else ByteOrder.LITTLE_ENDIAN;
+      Driver.BlockSize = Driver.config.blockSize;
+    }
+
+    def readDistancesAndWeights(isSammon: Boolean)
+    {
+      distances = BinaryReader2D.readRowRange(config.distanceMatrixFile, ParallelOps.procRowRange, ParallelOps.globalColCount, byteOrder, true, config.distanceTransform)
+      var w: Array[Array[Short]] = null
+      if (!Strings.isNullOrEmpty(config.weightMatrixFile)) {
+        w = BinaryReader2D.readRowRange(config.weightMatrixFile, ParallelOps.procRowRange, ParallelOps.globalColCount, byteOrder, true, null)
       }
+      weights = new WeightsWrap(w, distances, isSammon)
     }
-    return x;
-  }
 
-  def readConfigurations(cmd: CommandLine): Unit = {
-    Driver.config = ConfigurationMgr.LoadConfiguration(
-      cmd.getOptionValue(Constants.CmdOptionLongC)).damdsSection;
-    //TODO check if this is always correct
-    ParallelOps.globalColCount = config.numberDataPoints;
-    ParallelOps.nodeCount =
-      Integer.parseInt(cmd.getOptionValue(Constants.CmdOptionLongN));
-    ParallelOps.threadCount =
-      Integer.parseInt(cmd.getOptionValue(Constants.CmdOptionLongT));
-    ParallelOps.mmapsPerNode = if (cmd.hasOption(Constants.CmdOptionShortMMaps)) cmd.getOptionValue(Constants.CmdOptionShortMMaps).toInt else 1;
-    ParallelOps.mmapScratchDir = if (cmd.hasOption(Constants.CmdOptionShortMMapScrathDir)) cmd.getOptionValue(Constants.CmdOptionShortMMapScrathDir) else ".";
-
-    Driver.byteOrder =
-      if (Driver.config.isBigEndian) ByteOrder.BIG_ENDIAN else ByteOrder.LITTLE_ENDIAN;
-    Driver.BlockSize = Driver.config.blockSize;
-  }
-
-  private def readDistancesAndWeights(isSammon: Boolean) {
-    distances = BinaryReader2D.readRowRange(config.distanceMatrixFile, ParallelOps.procRowRange, ParallelOps.globalColCount, byteOrder, true, config.distanceTransform)
-    var w: Array[Array[Short]] = null
-    if (!Strings.isNullOrEmpty(config.weightMatrixFile)) {
-      w = BinaryReader2D.readRowRange(config.weightMatrixFile, ParallelOps.procRowRange, ParallelOps.globalColCount, byteOrder, true, null)
-    }
-    weights = new WeightsWrap(w, distances, isSammon)
-  }
-
-  def calculateStatisticsInternal(index: Int, iter: Iterator[IndexedRow]): Iterator[DoubleStatistics] = {
-    var result = List[DoubleStatistics]();
-    var missingDistCounts: Int = 0;
-    val stats: DoubleStatistics = new DoubleStatistics();
-    while (iter.hasNext) {
-      val cur = iter.next;
-      cur.vector.toArray.map(x => (if ((x * 1.0 / Short.MaxValue) < 0) (missingDistCounts += 1) else (stats.accept((x * 1.0 / Short.MaxValue)))))
-    }
-    result.::=(stats);
-    //TODO test missing distance count
-    missingDistCount += missingDistCounts
-    result.iterator
-  }
-
-  def combineStatistics(doubleStatisticsMain: DoubleStatistics, doubleStatisticsOther: DoubleStatistics): DoubleStatistics = {
-    doubleStatisticsMain.combine(doubleStatisticsOther)
-    doubleStatisticsMain
-  }
-
-  def generateVArrayInternal(index: Int, iter: Iterator[IndexedRow]): Iterator[DoubleStatistics] = {
-    null //TODO
-  }
-
-  def calculateStressInternal(preX: Array[Array[Double]], targetDimension: Int, tCur: Double,
-                              weights: WeightsWrap)(index: Int, iter: Iterator[IndexedRow]): Iterator[Double] = {
-    var result = List[Double]()
-    var diff: Double = 0.0
-
-    if (tCur > 10E-10) {
-      diff = Math.sqrt(2.0 * targetDimension) * tCur
-    }
-    //TODO support weightsWrap
-    val weight: Double = 1.0D
-    var localRowCount: Int = 0
-
-    while (iter.hasNext) {
-      var sigma: Double = 0.0
-      val cur = iter.next;
-      val globalRow = index + localRowCount;
-
-      cur.vector.toArray.zipWithIndex.foreach { case (element, index) => {
-        var origD = element * 1.0 / Short.MaxValue;
-        var euclideanD: Double = if (globalRow != index) calculateEuclideanDist(preX, targetDimension, globalRow, index) else 0.0;
-        val heatD: Double = origD - diff
-        val tmpD: Double = if (origD >= diff) heatD - euclideanD else -euclideanD
-        sigma += weight * tmpD * tmpD
+    def calculateStatisticsInternal(index: Int, iter: Iterator[IndexedRow]): Iterator[DoubleStatistics] = {
+      var result = List[DoubleStatistics]();
+      var missingDistCounts: Int = 0;
+      val stats: DoubleStatistics = new DoubleStatistics();
+      while (iter.hasNext) {
+        val cur = iter.next;
+        cur.vector.toArray.map(x => (if ((x * 1.0 / Short.MaxValue) < 0) (missingDistCounts += 1) else (stats.accept((x * 1.0 / Short.MaxValue)))))
       }
+      result.::=(stats);
+      //TODO test missing distance count
+      missingDistCount += missingDistCounts
+      result.iterator
+    }
+
+    def combineStatistics(doubleStatisticsMain: DoubleStatistics, doubleStatisticsOther: DoubleStatistics): DoubleStatistics = {
+      doubleStatisticsMain.combine(doubleStatisticsOther)
+      doubleStatisticsMain
+    }
+
+    def generateVArrayInternal(index: Int, iter: Iterator[IndexedRow]): Iterator[DoubleStatistics] = {
+      null //TODO
+    }
+
+    def calculateStressInternal(preX: Array[Array[Double]], targetDimension: Int, tCur: Double,
+                                weights: WeightsWrap)(index: Int, iter: Iterator[IndexedRow]): Iterator[Double] = {
+      var result = List[Double]()
+      var diff: Double = 0.0
+
+      if (tCur > 10E-10) {
+        diff = Math.sqrt(2.0 * targetDimension) * tCur
       }
+      //TODO support weightsWrap
+      val weight: Double = 1.0D
+      var localRowCount: Int = 0
 
-      localRowCount += 1
-      result.::=(sigma)
-    }
-    result.iterator
-  }
+      while (iter.hasNext) {
+        var sigma: Double = 0.0
+        val cur = iter.next;
+        val globalRow = index + localRowCount;
 
-  private def calculateEuclideanDist(vectors: Array[Array[Double]], targetDim: Int, i: Int, j: Int): Double = {
-    var dist: Double = 0.0;
-    for (k <- 0 until targetDim) {
-      val diff: Double = vectors(i)(k) - vectors(j)(k)
-      dist += diff * diff
+        cur.vector.toArray.zipWithIndex.foreach { case (element, index) => {
+          var origD = element * 1.0 / Short.MaxValue;
+          var euclideanD: Double = if (globalRow != index) calculateEuclideanDist(preX, targetDimension, globalRow, index) else 0.0;
+          val heatD: Double = origD - diff
+          val tmpD: Double = if (origD >= diff) heatD - euclideanD else -euclideanD
+          sigma += weight * tmpD * tmpD
+        }
+        }
+
+        localRowCount += 1
+        result.::=(sigma)
+      }
+      result.iterator
     }
-    dist = Math.sqrt(dist)
-    dist
+
+      def calculateEuclideanDist(vectors: Array[Array[Double]], targetDim: Int, i: Int, j: Int): Double = {
+        var dist: Double = 0.0;
+        for (k <- 0 until targetDim) {
+          val diff: Double = vectors(i)(k) - vectors(j)(k)
+          dist += diff * diff
+        }
+        dist = Math.sqrt(dist)
+        dist
+      }
   }
 }
 
